0f0d0ad389f38fd86f9724971f1050c6b66dacbf
Emery Barame <e.barame@alustudent.com>
2026-09-25 00:35:05 -0700

    feat: wire up Navbar and TaskManager

--- react-state-management/src/App.tsx
+++ react-state-management/src/App.tsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import Navbar from './components/Navbar'
+import TaskManager from './components/TaskManager'
+import { ThemeProvider } from './context/ThemeContext'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider>
+      <Navbar />
+      <TaskManager />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -115,7 +120,7 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
-    </>
+    </ThemeProvider>
   )
 }
 
