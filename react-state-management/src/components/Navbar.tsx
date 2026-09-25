71899a9331a8426602e90af21c162a0e109a8c12
Emery Barame <e.barame@alustudent.com>
2026-09-25 00:16:34 -0700

    fix: relocate files & fix import/type errors

--- react-state-management/src/components/Navbar.tsx
+++ react-state-management/src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useTheme } from "../context/ThemeContext";
 import styles from "./Navbar.module.css";
 import { DARK_THEME, LIGHT_THEME } from "../constants/theme";
